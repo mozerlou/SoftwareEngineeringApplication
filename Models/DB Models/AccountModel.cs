5ac679d9035efac497348972000976956eb54298
Louanne Mozer-Sallo <ljm054@shsu.edu>
2022-04-29 10:44:12 -0500

    ra

---
+++ Models/DB Models/AccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Models
+{
+    public class AccountModel
+    {
+        public int AccountID { get; set; }
+        public string AccountType { get; set; }
+        public string AccountBalance { get; set; }
+        public int CustomerID { get; set; }
+
+    }
+}
---
+++ Models/DB Models/TransactionsModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Models
+{
+    public class TransactionModels
+    {
+        public int TransactionID { get; set; }
+        public double TransactionAmount { get; set; }
+        public string BusinessName { get; set; }
+        public string BusinessLocation { get; set; }
+        public string ChargeType { get; set; }
+        public string TransactionType { get; set; }
+        public int TAccountID { get; set; }
+        public string TCustomerID { get; set; }
+
+        public string Safety { get; set; }
+    }
+}
