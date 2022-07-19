0a3e079faacd40364d44d4a7d05f20ba62e26cc3
Louanne Mozer-Sallo <ljm054@shsu.edu>
2022-07-18 20:42:02 -0500

    nnn

--- Startup.cs
+++ Startup.cs
@@ -23,13 +23,10 @@ namespace BlazorApp4
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<IDataAccess, DataAccess>();
             services.AddScoped<TokenProvider>();
         }
